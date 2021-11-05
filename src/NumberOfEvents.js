2089a42ceb8c1910e2fe4f142821995a4b107c26
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-11-05 00:20:46 -0700

    unit testing: feature 3 (specify the number of events)

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import RangeSlider from "react-bootstrap-range-slider";
+
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 12,
+  }
+
+  handleNumberChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      numberOfEvents: value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="events-range">
+        <h3>Adjust the number of events</h3>
+        <RangeSlider
+          min={1}
+          max={32}
+          className="number-on-range"
+          value={this.state.numberOfEvents}
+          onChange={this.handleNumberChange}
+        />
+      </div>
+    );
+  }
+}
+export default NumberOfEvents;
